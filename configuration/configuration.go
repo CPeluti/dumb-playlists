d77033d1cded73fd15f7d176d5284cf093bab440
Caio Peluti <cpeluti@gmail.com>
2026-04-09 14:02:43 -0300

    added module to read the configuration file

---
+++ configuration/configuration.go
@@ -0,0 +1,51 @@
+package configuration
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/goccy/go-yaml"
+)
+
+type Device struct {
+	Name      string   `yaml:"name" validate:"required"`
+	Path      string   `yaml:"path" validate:"required"`
+	Allowlist []string `yaml:"allowlist"`
+}
+
+type Config struct {
+	PlaylistsDir string   `yaml:"playlists_dir" validate:"required"`
+	Devices      []Device `yaml:"devices"`
+}
+
+func ReadFile(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	check(err)
+	return string(data), nil
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func ReadYaml(path string) (Config, error) {
+	var v Config
+	validate := validator.New()
+	file, _ := ReadFile(path)
+	dec := yaml.NewDecoder(
+		strings.NewReader(file),
+		yaml.Validator(validate),
+		yaml.Strict(),
+	)
+
+	if err := dec.Decode(&v); err != nil {
+		return v, err
+	} else {
+		fmt.Println(v)
+		return v, nil
+	}
+}
---
+++ configuration/configuration_test.go
@@ -0,0 +1,43 @@
+package configuration
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestConfiguration(t *testing.T) {
+	t.Run("read the yaml file", func(t *testing.T) {
+		got, err := ReadYaml("./fixtures/config.yaml")
+		want := Config{
+			PlaylistsDir: "/mnt/playlists",
+			Devices: []Device{
+				{
+					Name:      "teste",
+					Path:      "/mnt/teste",
+					Allowlist: []string{"playlist1", "playlist2"},
+				},
+				{
+					Name:      "teste2",
+					Path:      "/mnt/teste2",
+					Allowlist: []string{"playlist3", "playlist4"},
+				},
+			},
+		}
+		if err != nil {
+			t.Errorf("Got error %q", err)
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected %q but got %q", want, got)
+		}
+	})
+	t.Run("read the yaml file", func(t *testing.T) {
+		got, err := ReadYaml("./fixtures/invalid_config.yaml")
+		want := "Key: 'Config.PlaylistsDir' Error:Field validation for 'PlaylistsDir' failed on the 'required' tag"
+		if err == nil {
+			t.Errorf("Expected error but got none.")
+		}
+		if err.Error() != want {
+			t.Errorf("expected %q but got %q", want, got)
+		}
+	})
+}
